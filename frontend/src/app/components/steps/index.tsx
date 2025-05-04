489db2dbca9760337fbf2cdd1848961eef426aea
thejaysharma <jai3500000@gmail.com>
2025-05-05 01:11:31 +0530

    feat: added record description page

--- frontend/src/app/components/steps/index.tsx
+++ frontend/src/app/components/steps/index.tsx
@@ -1,38 +1,42 @@
+import { cn } from "@/lib/utils";
+
 export default function ProcessSteps({
   steps,
+  className,
 }: {
   steps: { title: string; description: string }[];
+  className?: string;
 }) {
   return (
-    <div className="w-full">
-      <div className="flex justify-center items-center gap-12">
+    <div className={cn("w-full flex justify-center px-20", className)}>
+      <div className="flex w-full">
         {steps.map((step, index) => (
           <div
             key={index}
-            className="flex flex-col items-center justify-center text-center mb-8"
+            className="flex-1 flex flex-col items-center relative"
           >
-            {/* Circle icon */}
-            <div className="relative flex items-center w-full">
-              <div className="flex justify-center items-center w-full">
-                <div className="size-12 rounded-full bg-sky-900 flex items-center justify-center text-white z-10">
-                  <div className="size-4 bg-white rounded-full" />
-                </div>
-              </div>
-              {/* Connecting dotted line */}
-              {index < steps.length - 1 && (
-                <div className="absolute top-6 left-32 h-0 border-t-3 border-dashed border-gray-400 w-full"></div>
-              )}
+            {/* Line to the next step */}
+            {index < steps.length - 1 && (
+              <div className="absolute top-5 right-0 w-1/2 border-t-2 border-dashed border-gray-400 z-0" />
+            )}
+            {index > 0 && (
+              <div className="absolute top-5 left-0 w-1/2 border-t-2 border-dashed border-gray-400 z-0" />
+            )}
+
+            {/* Circle */}
+            <div className="z-10 size-10 rounded-full bg-sky-900 flex items-center justify-center text-white">
+              <div className="size-3 bg-white rounded-full" />
             </div>
 
-            {/* Title */}
-            <span className="mt-4 text-sky-900 text-base font-bold font-['Montserrat'] leading-normal tracking-wide">
-              {step.title}
-            </span>
-
-            {/* Description */}
-            <p className="mt-2 self-stretch text-center justify-start text-neutral-900 text-sm font-medium font-['Montserrat'] leading-none tracking-wide">
-              {step.description}
-            </p>
+            {/* Text */}
+            <div className="mt-3 text-center mx-6">
+              <div className="text-sky-900 font-bold text-base">
+                {step.title}
+              </div>
+              <p className="mt-1 text-neutral-700 text-sm">
+                {step.description}
+              </p>
+            </div>
           </div>
         ))}
       </div>
